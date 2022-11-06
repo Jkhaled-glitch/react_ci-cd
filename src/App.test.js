f2d3c16341b68f4fb2350374a2fce60fedb8825e
Jkhaled-glitch <khaledjb584@gmail.com>
2022-11-06 19:37:35 +0100

    second commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react"
+import App from './App'
+import ReactDOM from "react-dom"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('render App', () => {
+  const div = ReactDOM.createRoot(document.createElement("div"))
+  div.render(<App />)
 });
